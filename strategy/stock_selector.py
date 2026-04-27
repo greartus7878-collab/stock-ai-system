0005ecb7c964950a094c26122f932d977c7a3ac3
greartus7878-collab <greartus7878@gmail.com>
2026-04-27 22:22:49 +0800

    Update stock_selector.py

--- strategy/stock_selector.py
+++ strategy/stock_selector.py
@@ -1,12 +1,15 @@
 stocks = {
-    "凯盛科技": 78,
-    "西藏珠峰": 73,
-    "奥拓电子": 55,
-    "吉林化纤": 49
+    "凯盛科技": 86,
+    "西藏珠峰": 82,
+    "中国卫通": 79,
+    "奥拓电子": 63,
+    "吉林化纤": 58
 }
 
-rank = sorted(stocks.items(), key=lambda x:x[1], reverse=True)
+rank = sorted(stocks.items(), key=lambda x: x[1], reverse=True)
 
-print("个股优先级：")
+print("今日个股Top5：")
 for i in rank:
     print(i[0], i[1])
+
+print("建议关注前2名")
